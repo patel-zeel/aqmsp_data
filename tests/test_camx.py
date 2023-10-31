047c1b54351062a3fd4a1e06d1bf522f180ba917
Utkarsh Mittal <utkarsh.mittal@iitgn.ac.in>
2023-11-01 00:06:45 +0530

    correction in directory name

--- tests/test_camx.py
+++ tests/test_camx.py
@@ -2,7 +2,7 @@ from aqmsp_data.preprocessing import preprocess_camx_met
 import xarray as xr
 
 def test_preprocess_raw_camx():
-    path = "camxmet2d.delhi.20231018.96hours.nc"
+    path = "/tests/camxmet2d.delhi.20231018.96hours.nc"
     ds = preprocess_camx_met(path,verbose=True)
     print(ds)
     assert isinstance(ds, xr.Dataset)
