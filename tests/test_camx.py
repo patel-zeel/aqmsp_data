40c3db4c0e5a5b1b61bc2c904fc3caa0b7ea0ad1
UtkarshMitta <utkarsh.mittal@iitgn.ac.in>
2023-11-10 15:53:02 +0530

    commit

--- tests/test_camx.py
+++ tests/test_camx.py
@@ -1,9 +1,9 @@
-from aqmsp_data.preprocessing import preprocess_camx_met
+from aqmsp_data import preprocessing
 import xarray as xr
 
 def test_preprocess_raw_camx():
     path = "tests/camxmet2d.delhi.20231018.96hours.nc"
-    ds = preprocess_camx_met(path,verbose=True)
+    ds = preprocessing.preprocess_camx_met(path,verbose=True)
     print(ds)
     assert isinstance(ds, xr.Dataset)
     return ds
